29f97809b9a20276926dcc2bd2cf2ae429645140
Tudor Marze <tudor-calin.marze@accenture.com>
2016-12-09 14:37:42 +0100

    Add camera permissions

--- example/Anyline/src/index.js
+++ example/Anyline/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  PermissionsAndroid,
   StyleSheet,
   Text,
   View,
@@ -15,10 +16,48 @@ class Anyline extends Component {
     AnylineOCR.setupScanViewWithConfigJson(JSON.stringify(config), 'ANALOG_METER', () => {}, () => {});
   }
 
+  requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.requestPermission(
+        PermissionsAndroid.PERMISSIONS.CAMERA
+      )
+      if (granted) {
+        console.log('Camera permission allowed');
+        this.openOCR();
+      } else {
+        console.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  hasCameraPermission = async () => {
+    try {
+      const result = await PermissionsAndroid.checkPermission(
+        PermissionsAndroid.PERMISSIONS.CAMERA);
+      return result;
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+  checkCameraPermissionAndOpen = () => {
+    this.hasCameraPermission().then((hasCameraPermission) => {
+      console.log('hasCameraPermission result is ' + hasCameraPermission);
+      if (hasCameraPermission) {
+        console.log('Opening OCR directly');
+        this.openOCR();
+      } else {
+        this.requestCameraPermission();
+      }
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text onPress={this.openOCR}>Open OCR reader.</Text>
+        <Text onPress={this.checkCameraPermissionAndOpen}>Open OCR reader.</Text>
       </View>
     );
   }
