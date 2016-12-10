6123a242e5dde2deac3f2a73e93960e4e7576be4
Michał Chudziak <michal.chudziak@gmail.com>
2016-12-10 17:16:46 +0100

    JS cleanup.

--- example/Anyline/src/index.js
+++ example/Anyline/src/index.js
@@ -7,18 +7,81 @@ import {
 } from 'react-native';
 import AnylineOCR from 'anyline-ocr-react-native-module';
 
+import Result from './Result';
+
 import config from '../config';
 
 class Anyline extends Component {
 
-  openOCR = () => {
-    AnylineOCR.setupScanViewWithConfigJson(JSON.stringify(config), 'ANALOG_METER', () => {}, () => {});
+  state = {
+    hasScanned: false,
+    result: '',
+    imagePath: '',
+    fullImagePath: '',
+    barcode: '',
+    scanMode: '',
+    meterType: '',
+    cutoutBase64: '',
+    fullImageBase64: '',
   }
 
+  openOCR = () => {
+    AnylineOCR.setupScanViewWithConfigJson(
+      JSON.stringify(config),
+      'ANALOG_METER',
+      this.onResult,
+      this.onError,
+    );
+  }
+
+  onResult = (dataString) => {
+    const data = JSON.parse(dataString);
+
+    this.setState({
+      hasScanned: true,
+      result: data.reading,
+      imagePath: data.imagePath,
+      fullImagePath: data.fullImagePath,
+      scanMode: data.scanMode,
+      meterType: data.meterType,
+      cutoutBase64: data.cutoutBase64,
+      fullImageBase64: data.fullImageBase64,
+    });
+  };
+
+  onError = (error) => {
+    alert(error);
+  };
+
   render() {
+    const {
+      hasScanned,
+      result,
+      imagePath,
+      fullImagePath,
+      barcode,
+      scanMode,
+      meterType,
+      cutoutBase64,
+      fullImageBase64,
+    } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text onPress={this.openOCR}>Open OCR reader.</Text>
+        {hasScanned ? (
+          <Result
+            result={result}
+            imagePath={imagePath}
+            fullImagePath={fullImagePath}
+            barcode={barcode}
+            scanMode={scanMode}
+            meterType={meterType}
+            cutoutBase64={cutoutBase64}
+            fullImageBase64={fullImageBase64}
+          />
+        ) : (
+          <Text onPress={this.openOCR}>Open OCR reader.</Text>
+        )}
       </View>
     );
   }
