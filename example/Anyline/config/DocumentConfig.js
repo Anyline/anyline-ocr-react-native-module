e8a866d6037e6b859c07dc7962267b333da9b155
Jonas <jonas@anyline.io>
2017-08-18 15:45:47 +0200

    changed Doc View Config, changed Orientation Lock

--- example/Anyline/config/DocumentConfig.js
+++ example/Anyline/config/DocumentConfig.js
@@ -1,39 +1,41 @@
 export default {
     license: 'eyAiYW5kcm9pZElkZW50aWZpZXIiOiBbICJjb20uYW55bGluZSIgXSwgImRlYnVn\nUmVwb3J0aW5nIjogIm9uIiwgImlvc0lkZW50aWZpZXIiOiBbICJjb20uYW55bGlu\nZSIgXSwgImxpY2Vuc2VLZXlWZXJzaW9uIjogMiwgIm1ham9yVmVyc2lvbiI6ICIz\nIiwgInBpbmdSZXBvcnRpbmciOiB0cnVlLCAicGxhdGZvcm0iOiBbICJBbmRyb2lk\nIiwgImlPUyIgXSwgInNjb3BlIjogWyAiQUxMIiBdLCAic2hvd1dhdGVybWFyayI6\nIHRydWUsICJ0b2xlcmFuY2VEYXlzIjogMywgInZhbGlkIjogIjIwMTctMTItMzEi\nIH0KcGNRSXBOU0MzdGZ5QVV1RkRtSEMyMGo0TElXSGVTMnNxRmJybllqOWZDOWZm\nS1ZpaWVGSitLUGZGY2tTWm9YUgpMVmZvWGx2QmZDOVVaaUErZzZtTGNTV3owUVVl\nSkVuRHBGcWJVU0cxVGVBN1NJenR2aU9BOGg2bFlnNXFSZmw0CjQ5d0IvNXVtL2Ir\nUDhHaXRLUzMzaFNacVhTbEdjbnkxYUVRaWxCRjFHL2NCeitIcUFtUEFMaVpBVzFQ\nZDkxdDYKZXZTcU1XdkNtMk01blZUaVNDd2dDZnR6Zkg3WENnQlVCU2hOb2ZoWlFJ\nVCtiUE5ycTE3SGg1YkVWa2ZoUlZBSQpSc0d1YlM4SEpvVlAvaEs5a2FMdzliRWYx\nZXpYZVpVOXFUbCswSXZKUy9lSlBrazJmV0IxcS9CUzM4ZFBVOTZHClpqVXIyRmlQ\nRjNSc1FxRC9UU244Q0E9PQo=',
     options: {
-        "captureResolution":"1080",
-        "pictureResolution":"1080p",
-        "cutout": {
-            "style": "rect",
-            "maxWidthPercent": "100%",
-            "maxHeightPercent": "100%",
-            "width": 1080,
-            "ratioFromSize": {
-                "width": 10,
-                "height": 15
-            },
-            "alignment": "center",
-            "strokeWidth": 2,
-            "cornerRadius": 0,
-            "strokeColor": "00000000"
+      "captureResolution":"1080",
+      "pictureResolution":"photo",
+      "cutout": {
+        "style": "rect",
+        "maxWidthPercent": "100%",
+        "maxHeightPercent": "100%",
+        "ratioFromSize": {
+          "width": 10,
+          "height": 15
         },
-        "flash": {
-            "mode": "manual",
-            "alignment": "bottom_left",
-            "offset": {
-                "x": 10,
-                "y": 0
-            }
-        },
-        "visualFeedback" : {
-            "style": "RECT",
-            "strokeColor": "300099FF",
-            "animationDuration": 150,
-            "cornerRadius": 2
-        },
-        "beepOnResult": true,
-        "vibrateOnResult": true,
-        "blinkAnimationOnResult": true,
-        "cancelOnResult": true
+        "alignment": "center",
+        "strokeWidth": 2,
+        "cornerRadius": 0,
+        "outerAlpha": 0.0,
+        "outerColor": "00000000",
+        "strokeColor": "00000000"
+      },
+      "flash": {
+        "mode": "manual",
+        "alignment": "bottom_left",
+        "offset": {
+          "x": 10,
+          "y": 0
+        }
+      },
+      "visualFeedback" : {
+        "style": "RECT",
+        "strokeColor": "FF0000",
+        "animationDuration": 150,
+        "cornerRadius": 2,
+        "strokeWidth": 4,
+      },
+      "beepOnResult": false,
+      "vibrateOnResult": false,
+      "blinkAnimationOnResult": true,
+      "cancelOnResult": true
     }
 }
