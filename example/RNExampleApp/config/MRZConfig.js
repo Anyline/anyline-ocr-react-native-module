6ff5d5150cd846d1e119e4d99720c9ec1bcea473
Gerhard <gerhard@anyline.com>
2019-05-09 17:28:11 +0200

    json changes: viz

--- example/RNExampleApp/config/MRZConfig.js
+++ example/RNExampleApp/config/MRZConfig.js
@@ -30,8 +30,8 @@ export default {
             "strictMode": false,
             "cropAndTransformID": false,
 	    "mrzFieldScanOptions": {
-            	"dateOfIssue" : "optional",
-                "address" : "optional"
+            	"vizDateOfIssue" : "optional",
+                "vizAddress" : "optional"
             }
           }
         }
