76848cc8b10de9e156cdf773e25e4e4fc6cb46bc
Gerhard <gerhard@anyline.com>
2020-09-18 13:35:48 +0200

    document scanner update: progress dialog closes

--- plugin/android/src/main/java/com/anyline/reactnative/Document4Activity.java
+++ plugin/android/src/main/java/com/anyline/reactnative/Document4Activity.java
@@ -3,10 +3,12 @@ package com.anyline.reactnative;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.PointF;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
@@ -313,11 +315,40 @@ public class Document4Activity extends AnylineBaseActivity implements CameraOpen
             @Override
             public void onTakePictureSuccess() {
                 // this is called after the image has been captured from the camera and is about to be processed
+                if (progressDialog != null && progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+
+                // do not display error messages while "processing picture" progress dialog is shown:
+                errorMessageLayout.setVisibility(View.GONE);
+
                 progressDialog = ProgressDialog.show(Document4Activity.this, getString(
                         getResources().getIdentifier("document_processing_picture_header", "string", getPackageName())),
                                                      getString(
                                                              getResources().getIdentifier("document_processing_picture", "string", getPackageName())),
                                                      true);
+
+                // there is a bug in the sdk that onTakePictureSuccess is called but onResult not.
+                // so implement a workaround: hide the progressDialog after 2 seconds, the phone will continue scanning
+                final Handler handler1 = new Handler();
+                final Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        if (progressDialog.isShowing()) {
+                            progressDialog.dismiss();
+                        }
+                    }
+                };
+
+                progressDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        handler1.removeCallbacks(runnable);
+                    }
+                });
+
+                handler1.postDelayed(runnable, 2000);
+                // workaround end
 
                 if (errorMessageAnimator != null && errorMessageAnimator.isRunning()) {
 
