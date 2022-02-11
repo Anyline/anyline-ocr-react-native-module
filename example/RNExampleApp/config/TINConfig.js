470da87accb3a4dc718eac0797473db073d17f29
Renato Neves Ribeiro <renato.neves@anyline.com>
2022-02-11 15:08:13 +0100

    update license, indent unaligned json files

--- example/RNExampleApp/config/TINConfig.js
+++ example/RNExampleApp/config/TINConfig.js
@@ -1,49 +1,49 @@
 export default {
-    license: 'ewogICJsaWNlbnNlS2V5VmVyc2lvbiI6IDIsCiAgImRlYnVnUmVwb3J0aW5nIjogIm9uIiwKICAiaW1hZ2VSZXBvcnRDYWNoaW5nIjogZmFsc2UsCiAgIm1ham9yVmVyc2lvbiI6ICIyNSIsCiAgIm1heERheXNOb3RSZXBvcnRlZCI6IDUsCiAgImFkdmFuY2VkQmFyY29kZSI6IHRydWUsCiAgIm11bHRpQmFyY29kZSI6IHRydWUsCiAgInN1cHBvcnRlZEJhcmNvZGVGb3JtYXRzIjogWwogICAgIkFMTCIKICBdLAogICJwaW5nUmVwb3J0aW5nIjogdHJ1ZSwKICAicGxhdGZvcm0iOiBbCiAgICAiaU9TIiwKICAgICJBbmRyb2lkIgogIF0sCiAgInNjb3BlIjogWwogICAgIkFMTCIKICBdLAogICJzaG93UG9wVXBBZnRlckV4cGlyeSI6IGZhbHNlLAogICJzaG93V2F0ZXJtYXJrIjogdHJ1ZSwKICAidG9sZXJhbmNlRGF5cyI6IDkwLAogICJ2YWxpZCI6ICIyMDIyLTEyLTMxIiwKICAiaW9zSWRlbnRpZmllciI6IFsKICAgICJjb20uYW55bGluZS5leGFtcGxlLnJlYWN0bmF0aXZlIiwKICAgICJjb20uYW55bGluZS5leGFtcGxlLnJlYWN0bmF0aXZlLmJldGEiCiAgXSwKICAiYW5kcm9pZElkZW50aWZpZXIiOiBbCiAgICAiY29tLmFueWxpbmUuZXhhbXBsZS5yZWFjdG5hdGl2ZSIKICBdCn0KS2RiZmVncWJweG13R2tOVnl1aXNFSUh0L2pGS0NrT2ZjeXlmYjIrbWtURVpwdGMyVWtUbkduZlgvTWJpWmRPdy9pVjFOMVJNMzFjdmp1dEMvbkxZRmxheWUyYTdHTnRpTjhnNmRTZCt6eXZOcktUb29rZFkwRFVWaGFxUko4WHcxeVIzbTd1OXoyb2NwTHl6WmVpa0J2cDJGWjRoWjZScDQveUgrbTFxT01lRmhaaHo3Um5pQnNSa1RjcUNhVW9LRDZJSUY2djc3SGxYdUUvV0VacWFEdFNkaXpmOFRUM2ljTjhkcEdmTzZEZmtJVkx0TWhjK3lTMEdJWHVXbFU1aTlESTVtMGM3SFRnQVMwOWg4dGFsbjl5R2xKQUFtY1VoL2NpdUtCZmlhd2pycFRLSm9FOTBUZjFoSGZIQUxpSzlNcnYwckdJZUNwOHg1STlzOHBtU3d3PT0=',
-  options: {
-      "camera": {
-          "captureResolution": "720p"
-      },
-      "flash": {
-          "mode": "manual",
-          "alignment": "bottom_right"
-      },
-      "viewPlugin" : {
-          "plugin": {
-            "id": "TIN",
-            "ocrPlugin": {
-              "tinConfig":{
-                  "scanMode" : "UNIVERSAL"
-              }
-            }
-          },
-          "cutoutConfig" : {
-              "style": "rect",
-              "width": 720,
-              "alignment": "center",
-              "maxWidthPercent": "80%",
-              "ratioFromSize": {
-                  "width": 720,
-                  "height": 144
-              },
-              "outerColor": "000000",
-              "outerAlpha": 0.3,
-              "strokeWidth": 2,
-              "strokeColor": "FFFFFF",
-              "cornerRadius": 2,
-              "feedbackStrokeColor": "0099FF"
-          },
-          "scanFeedback" : {
-              "animation": "traverse_multi",
-              "animationDuration" : 250,
-              "style": "contour_rect",
-              "strokeWidth": 2,
-              "strokeColor": "0099FF",
-              "beepOnResult": true,
-              "vibrateOnResult": true,
-              "blinkAnimationOnResult": true
-          },
-          "cancelOnResult" : true
-      }
-  }
+    license: 'ewogICJsaWNlbnNlS2V5VmVyc2lvbiI6ICIzLjAiLAogICJkZWJ1Z1JlcG9ydGluZyI6ICJwaW5nIiwKICAibWFqb3JWZXJzaW9uIjogIjM3IiwKICAic2NvcGUiOiBbCiAgICAiQUxMIgogIF0sCiAgIm1heERheXNOb3RSZXBvcnRlZCI6IDUsCiAgImFkdmFuY2VkQmFyY29kZSI6IHRydWUsCiAgIm11bHRpQmFyY29kZSI6IHRydWUsCiAgInN1cHBvcnRlZEJhcmNvZGVGb3JtYXRzIjogWwogICAgIkFMTCIKICBdLAogICJwbGF0Zm9ybSI6IFsKICAgICJpT1MiLAogICAgIkFuZHJvaWQiCiAgXSwKICAic2hvd1dhdGVybWFyayI6IHRydWUsCiAgInRvbGVyYW5jZURheXMiOiAzMCwKICAidmFsaWQiOiAiMjAyMi0xMi0zMSIsCiAgImlvc0lkZW50aWZpZXIiOiBbCiAgICAiY29tLmFueWxpbmUuZXhhbXBsZS5yZWFjdG5hdGl2ZSIsCiAgICAiY29tLmFueWxpbmUuZXhhbXBsZS5yZWFjdG5hdGl2ZS5iZXRhIgogIF0sCiAgImFuZHJvaWRJZGVudGlmaWVyIjogWwogICAgImNvbS5hbnlsaW5lLmV4YW1wbGUucmVhY3RuYXRpdmUiCiAgXQp9CklibmlieWFiVTRmWFNwRDZiYU1CUlRiUUQvQUxHTzI0cWdWUHM4Q3FWdThET0dQQnBlRGs4UjhYNTF6WWMvOW95TWZjbWFtakpKSktvMmM2ZVpJWGticzkzNkdqM1M2STBIS2UycEVPd1FNdGZQYVBWY3hKOWJHT25venF6UFk1bWUwNGtJYk9TYVhka3A0SUcwOGM0ZWpqTitsS0N6Qnd6ekhVSkUyNDJ1dnFvKy9tZmFWS2E3T0trOUdWaWYvQzBzamFPNHhhRTFlcHRQT2lKYkJlSCtpK1RCWnFJZDQxamZIdTl1NHEwOHozM0ZuaG5yU0hpMFQwL3VCakFLYStsRGpzbXJPbFBiaG5tU0V6K3RBUVE5NEhPcHA2OTd6b2hCak85WENDZE81aWJwR3pMekhoYWZ3T3VVVy9OQ1NDWFNsNjAzVDE0QitOVjUydWZ4eFlxQT09',
+    options: {
+        "camera": {
+            "captureResolution": "720p"
+        },
+        "flash": {
+            "mode": "manual",
+            "alignment": "bottom_right"
+        },
+        "viewPlugin" : {
+            "plugin": {
+                "id": "TIN",
+                "ocrPlugin": {
+                    "tinConfig":{
+                        "scanMode" : "UNIVERSAL"
+                    }
+                }
+            },
+            "cutoutConfig" : {
+                "style": "rect",
+                "width": 720,
+                "alignment": "center",
+                "maxWidthPercent": "80%",
+                "ratioFromSize": {
+                    "width": 720,
+                    "height": 144
+                },
+                "outerColor": "000000",
+                "outerAlpha": 0.3,
+                "strokeWidth": 2,
+                "strokeColor": "FFFFFF",
+                "cornerRadius": 2,
+                "feedbackStrokeColor": "0099FF"
+            },
+            "scanFeedback" : {
+                "animation": "traverse_multi",
+                "animationDuration" : 250,
+                "style": "contour_rect",
+                "strokeWidth": 2,
+                "strokeColor": "0099FF",
+                "beepOnResult": true,
+                "vibrateOnResult": true,
+                "blinkAnimationOnResult": true
+            },
+            "cancelOnResult" : true
+        }
+    }
 }
